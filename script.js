4ab2174647de0731991c03302661b44815eae281
TurquoiseWater <TurquoiseWater@users.noreply.github.com>
2017-10-29 23:28:22 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 (() => {
-
+	
   const companies = [
     {id: 1, name: 'Amazon', location: 'Seattle'},
     {id: 2, name: 'Apple', location: 'Cupertino'},
@@ -8,5 +8,30 @@
     {id: 5, name: 'Leeroy', location: 'Sundsvall'},
     {id: 6, name: 'Tesla', location: 'Palo Alto'}
   ]
+ 
+  var listOfCompanies = $('ul#list')
+    
+  // iterate through the companies
+  var worldsBestCompanies = $.map(companies, function(company, idx) {
+	  
+			var li = $('<li/>')
+				.addClass('li-item')
+				.attr('role', 'listitem')
+				.appendTo(listOfCompanies);
+			var liText = $('</p>')
+				.addClass('li-name')
+				.attr('role', 'text')
+				.text(companies[idx].name + ", " + company.location)
+				.click(function() {
+				 alert("You clicked on: " + company.name);
+				})
+				.appendTo(li);
 
+  });
+
+	
+	
 })()
+
+
+
