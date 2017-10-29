35e0c82fc4e649ed3c78c671d82e974e7efc99ea
TurquoiseWater <TurquoiseWater@users.noreply.github.com>
2017-10-29 23:26:39 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 (() => {
-
+	
   const companies = [
     {id: 1, name: 'Amazon', location: 'Seattle'},
     {id: 2, name: 'Apple', location: 'Cupertino'},
@@ -8,5 +8,22 @@
     {id: 5, name: 'Leeroy', location: 'Sundsvall'},
     {id: 6, name: 'Tesla', location: 'Palo Alto'}
   ]
+
+	var comp = companies[0].name;
+	var loc = companies[0].location;
+				
+	var listOfCompanies = $('ul#list')
+	$.each(companies, function(i)
+	{
+		var li = $('<li/>')
+			.addClass('li-item')
+			.attr('role', 'listitem')
+			.appendTo(listOfCompanies);
+		var liText = $('</p>')
+			.addClass('li-name')
+			.attr('role', 'text')
+			.text(companies[i].name + " - " + companies[i].location)
+			.appendTo(li);
+	});
 
 })()
